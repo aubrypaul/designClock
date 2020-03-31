e6fbe5127da5cd29a7f22a088a5d3089c52359a2
Paul Aubry <paulaubry@macbook-pro-de-paul.home>
2020-03-31 14:19:47 +0200

    2.0

--- sketch.js
+++ sketch.js
@@ -1,15 +1,43 @@
 function setup(){
   createCanvas(400,400);
-
+  angleMode(DEGREES);
+}
 
 function draw(){
   background(0);
 
   let hr = hour();
   let mn = minute();
-  let sc = seconde();
+  let sc = second();
+  translate(width/2,height/2);
+  rotate(-90);
 
-  fill(255);
-  text(hr + ':' + mn + ':' + sc, 10,200);
 
+  strokeWeight(8);
+  noFill();
+
+  stroke(255,100,150);
+  let end1 = map(sc, 0,60, 0,360);
+  arc(0,0,300,300,0,end1);
+
+  line(0,0,100*cos(end1),100*sin(end1))
+
+  stroke(150,100,255);
+  let end2 = map(mn, 0,60, 0,360);
+  arc(0,0,280,280,0,end2);
+
+  line(0,0,75*cos(end2),75*sin(end2))
+
+  stroke(150,255,100);
+  let end3 = map(hr % 12,0,12, 0,360);
+  arc(0,0,260,260,0,end3);
+
+  line(0,0,50*cos(end3),50*sin(end3))
+
+  stroke(255);
+  point(0,0);
+
+  // fill(255);
+  // noStroke();
+  // text(hr + ':' + mn + ':' + sc, 10,200);
 }
